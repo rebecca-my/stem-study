f6416a5393ef5d36be1f3b929578cf9ed56579ae
rebecca-my <rebecca.sc.myers@gmail.com>
2020-10-24 10:34:14 -0400

    adding skip gram model

---
+++ sk_model.py
@@ -0,0 +1,47 @@
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+import tensorflow as tf
+import tensorflow.keras as keras
+from tensorflow.keras.layers import Dense, Input, Embedding, Dot
+from tensorflow.keras.optimizers import Adagrad
+from tensorflow.keras.models import Model
+from tensorflow.keras.callbacks import ModelCheckpoint
+import os
+
+class SkipGramModel ():
+    def __init__(self, optimizer=Adagrad(learning_rate=0.02879), embed_dim=218, simple=True):
+        self.optimizer = optimizer
+        self.embed_dim = embed_dim
+        self.simple = simple
+
+        self.model = self.__build_model()
+
+    def __build_model(self):
+        input_course_ = Input(shape=(1,), name='Course_ids')
+        input_context_ = Input(shape=(1,), name='Context')
+        embed = Embedding(len(self.embedding_id), self.embed_dim, name='Course_embedding')(input_course_)
+        embed2 = Embedding(len(self.embedding_id), self.embed_dim, name='Context_embedding')(input_context_)
+        output = Dot(-1)([embed, embed2])
+        sigmoid = keras.activations.sigmoid(output)
+
+        self.model = Model(inputs=[input_course_, input_context_],outputs=sigmoid, name='Model')
+        print(self.model.summary())
+        self.model.compile(loss='binary_crossentropy', optimizer=self.optimizer, metrics=['acc'])
+
+        return self.model
+
+    def train(self, filepath=None):
+        if filepath is not None:
+            checkpoint = ModelCheckpoint(filepath, monitor='val_loss', verbose=1, save_best_only=True, mode='min')
+            callbacks_list = [checkpoint]
+        else:
+            callbacks_list = None
+        self.model.fit(train_generator(), validation_data=valid_generator(), steps_per_epoch = len(self.train_students),
+                validation_steps = len(self.valid_students), epochs = 150)
+
+    def save(self, fname):
+        self.model.save_weights(os.path.join('skip_gram_weights', fname))
+
+    def load(self, fname):
+        path = os.path.join('skip_gram_weights', fname)
+        self.model.load_weights(path, by_name=True)
