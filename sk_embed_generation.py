fe85c28a37f930b26e954f1aac417b2c4e95f29f
rebecca-my <rebecca.sc.myers@gmail.com>
2020-10-26 10:08:14 -0400

    combined embed_gen and data parser

--- sk_embed_generation.py
+++ sk_embed_generation.py
@@ -2,11 +2,13 @@ import pandas as pd
 import numpy as np
 
 class EmbeddingGenerator ():
-    def __init__(self, crs_df, course_to_id):
-        self.crs_df = crs_df
-        self.course_to_id = course_to_id
-        self.unique_students = unique_students
-        self.courses_set = courses_set
+    def __init__(self, folder_path):
+        self.folder_path = folder_path
+
+        self._load_courses()
+        self.embedding_id = list(self.crs_df['agg_id'].unique())
+        self.course_to_id = dict([(name, i) for i, name in enumerate(self.embedding_id)])
+        self.unique_students = list(self.crs_df['PRSN_UNIV_ID'].unique())
 
         np.random.seed(9)
         np.random.shuffle(self.unique_students)
@@ -16,20 +18,49 @@ class EmbeddingGenerator ():
         self.valid_students = self.unique_students[self.n_train : self.n_train + self.n_valid]
         self.crs_df_train = self.crs_df[self.crs_df['PRSN_UNIV_ID'].isin(self.train_students)]
         self.crs_df_valid = self.crs_df[self.crs_df['PRSN_UNIV_ID'].isin(self.valid_students)]
-		
+
+    def make_set(self, df):
+        return set(df['agg_id'].map(self.course_to_id))
+
+    def _load_courses(self):
+        courses = pd.read_csv(self.folder_path, encoding='latin')
+        mask_type = courses['CRS_TYPE']=='ENRL'
+        discarded_grades = ['ZZ']
+        mask_grade = ~courses['CRS_OFCL_GRD_CD'].isin(discarded_grades)                          
+        mask = mask_grade&mask_type
+        crs_embed = courses[mask]
+        
+        self.crs_df = pd.DataFrame(crs_embed)
+        self.crs_df['agg_id'] = self.crs_df['CRS_ID'].astype(str)
+
+
+        subj_by_id = self.crs_df['agg_id'].value_counts()
+        big_subj_by_id = subj_by_id[subj_by_id>10].index
+        self.crs_df['agg_id'] = np.where(self.crs_df['agg_id'].isin(big_subj_by_id), self.crs_df['agg_id'],'not_in_final_results')
+
+        orphan_classes = self.crs_df[self.crs_df['agg_id']=='not_in_final_results']
+        crs_dept_cd = orphan_classes['CRS_SUBJ_DEPT_CD'].value_counts()
+        big_dept = crs_dept_cd[crs_dept_cd>10].index
+        self.crs_df['agg_id'] = np.where(self.crs_df['agg_id'] != 'not_in_final_results', self.crs_df['agg_id'],
+                                                np.where(self.crs_df['CRS_SUBJ_DEPT_CD'].isin(big_dept),
+                                                self.crs_df['CRS_SUBJ_DEPT_CD'], 'other_dept'))
+
+    def course_to_label(self, course):
+        return self.crs_df[self.crs_df['agg_id']==course].iloc[0]['CRS_NM']
+
 
     def train_generator(self): 
         negative_courses = self.crs_df['agg_id'].map(self.course_to_id)
         n_neg = len(negative_courses)
         while True:
             for (student, term), df in self.crs_df_train.groupby(['PRSN_UNIV_ID','ACAD_TERM_CD']):
-                self.courses_set = make_set(df)
-                if len(self.courses_set) > 1:
-                    for crs_1 in self.courses_set:
+                courses_set = self.make_set(df)
+                if len(courses_set) > 1:
+                    for crs_1 in courses_set:
                         contexts = []
                         courses_x = []
                         matches = []
-                        for crs_2 in self.courses_set: 
+                        for crs_2 in courses_set: 
                             x = crs_1
                             y = crs_2
                             if x!=y:
@@ -49,13 +80,13 @@ class EmbeddingGenerator ():
         n_neg = len(negative_courses)
         while True:
             for (student, term), df in self.crs_df_valid.groupby(['PRSN_UNIV_ID','ACAD_TERM_CD']):
-                self.courses_set = make_set(df)
-                if len(self.courses_set) > 1:
-                    for crs_1 in self.courses_set:
+                courses_set = self.make_set(df)
+                if len(courses_set) > 1:
+                    for crs_1 in courses_set:
                         contexts = []
                         courses_x = []
                         matches = []
-                        for crs_2 in self.courses_set: 
+                        for crs_2 in courses_set: 
                             x = crs_1
                             y = crs_2
                             if x!=y:
@@ -69,4 +100,3 @@ class EmbeddingGenerator ():
                         courses_x = np.concatenate(courses_x, axis=0)
                         matches = np.concatenate(matches, axis=0)
                         yield [contexts, courses_x], matches
-                        
